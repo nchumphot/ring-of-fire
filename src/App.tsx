72483e8c8b15765c780331d6a424684c47b5ca1f
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-02-22 15:55:26 +0000

    Added states and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { ICard } from "./interfaces/ICard";
+import { IPlayer } from "./interfaces/IPlayer";
+import { GamePlay } from "./routes/GamePlay";
+import { HomePage } from "./routes/HomePage";
+import { generateCards } from "./utils/generateCards";
 
-function App() {
+function App(): JSX.Element {
+  const [players, setPlayers] = useState<IPlayer[]>([]);
+  const newCards = generateCards();
+  const [cards, setCards] = useState<ICard[]>(newCards);
+  console.log(cards);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage {...{ players, setPlayers }} />} />
+          <Route path="/start" element={<GamePlay />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
