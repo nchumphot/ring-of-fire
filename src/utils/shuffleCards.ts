c91f2763728e54c50da04dbabbca9b12d0ccd596
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-02-25 18:58:30 +0000

    Shuffling now includes the last card

--- src/utils/shuffleCards.ts
+++ src/utils/shuffleCards.ts
@@ -4,7 +4,7 @@ export function shuffleCards(current: ICard[]): ICard[] {
   let oldDeck = current;
   const newDeck = [];
   for (let i = current.length - 1; i >= 0; i--) {
-    const j = Math.floor(Math.random() * i);
+    const j = Math.floor(Math.random() * (i + 1));
     newDeck.push(oldDeck[j]);
     oldDeck.splice(j, 1);
   }
