f411895518e71f8613fa73995900bbaff994c1db
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-02-25 15:14:05 +0000

    Added a component for adding a new rule

---
+++ src/components/AddARule.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export function AddARule(props: {
+  setRules: React.Dispatch<React.SetStateAction<string[]>>;
+}): JSX.Element {
+  const [typedRule, setTypedRule] = useState<string>("");
+  const [ruleAdded, setRuleAdded] = useState<boolean>(false);
+  if (ruleAdded === false) {
+    return (
+      <form>
+        <label htmlFor="rule">Add a rule</label>
+        <input
+          name="rule"
+          type="text"
+          placeholder="Type your rule here..."
+          value={typedRule}
+          onChange={(e) => setTypedRule(e.target.value)}
+        ></input>
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={() => {
+            props.setRules((prev) => [...prev, typedRule]);
+            setRuleAdded(true);
+          }}
+        >
+          Add rule
+        </button>
+      </form>
+    );
+  } else {
+    return <p className="text-danger">{`New rule: ${typedRule}`}</p>;
+  }
+}
