0de7064d66393fef3b38faab4f0e1734494ec01e
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-02-25 17:37:09 +0000

    Put dashboard in columns

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -21,59 +21,108 @@ export function Dashboard(props: {
     setNoOfKings(props.cards.filter((c) => c.rank === "K").length);
   }, [props.cards]);
   return (
-    <div>
-      <h2>Game Information</h2>
-      <p>Current player: {props.currentPlayer.name}</p>
-      <p>There are {props.cards.length} cards left.</p>
-      <p>{`There are ${noOfKings} King${noOfKings > 1 && "s"} left.`}</p>
-      <h3>❓ Question Master</h3>
-      <p>
-        {props.questionMaster.length !== 0
-          ? props.questionMaster[0].name
-          : "None"}
-      </p>
-      <h3>👍🏼 Thumb Master</h3>
-      <p>
-        {props.thumbMaster.length !== 0 ? props.thumbMaster[0].name : "None"}
-      </p>
-      <button
-        type="button"
-        className="btn btn-warning"
-        onClick={() => props.setThumbMaster([])}
-      >
-        Reset
-      </button>
-      <h3>😇 Heaven Master</h3>
-      <p>
-        {props.heavenMaster.length !== 0 ? props.heavenMaster[0].name : "None"}
-      </p>
-      <button
-        type="button"
-        className="btn btn-warning"
-        onClick={() => props.setHeavenMaster([])}
-      >
-        Reset
-      </button>
-      <h3>Rules</h3>
-      {props.rules.length === 0 ? (
-        <p>None</p>
-      ) : (
-        <ul>
-          {props.rules.map((r) => (
-            <li>{r}</li>
-          ))}
-        </ul>
-      )}
-      <h3>Mates</h3>
-      {props.teams.length === 0 ? (
-        <p>None</p>
-      ) : (
-        <ul>
-          {props.teams.map((t) => (
-            <li>{getTeamMemberNames(t)}</li>
-          ))}
-        </ul>
-      )}
+    <div className="container-lg">
+      <div className="row">
+        <div className="col m-2 p-2 border border-dark">
+          <p>Current player: {props.currentPlayer.name}</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col m-2 p-2 border border-dark">
+          <h3>The Deck</h3>
+          <p>There are {props.cards.length} cards left.</p>
+          <p>Probability of breaking: </p>
+        </div>
+        <div
+          className={`col m-2 p-2 border border-${
+            noOfKings > 1 ? "dark" : "danger"
+          }`}
+        >
+          <h3>The King's Cup</h3>
+          <p>{`There are ${noOfKings} King${noOfKings > 1 && "s"} left.`}</p>
+          <p>{`Probability of getting the King's cup: ${
+            noOfKings > 1 ? "0" : (noOfKings / props.cards.length).toString()
+          }`}</p>
+        </div>
+      </div>
+      <div className="row">
+        <div
+          className={`col m-2 p-2 border border-${
+            props.questionMaster.length !== 0 ? "danger" : "dark"
+          }`}
+        >
+          <h3>❓ Question Master</h3>
+          <p>
+            {props.questionMaster.length !== 0
+              ? props.questionMaster[0].name
+              : "None"}
+          </p>
+        </div>
+        <div
+          className={`col m-2 p-2 border border-${
+            props.thumbMaster.length !== 0 ? "danger" : "dark"
+          }`}
+        >
+          <h3>👍🏼 Thumb Master</h3>
+          <p>
+            {props.thumbMaster.length !== 0
+              ? props.thumbMaster[0].name
+              : "None"}
+          </p>
+          <button
+            type="button"
+            className="btn btn-warning btn-sm"
+            onClick={() => props.setThumbMaster([])}
+          >
+            Reset
+          </button>
+        </div>
+        <div
+          className={`col m-2 p-2 border border-${
+            props.heavenMaster.length !== 0 ? "danger" : "dark"
+          }`}
+        >
+          <h3>😇 Heaven Master</h3>
+          <p>
+            {props.heavenMaster.length !== 0
+              ? props.heavenMaster[0].name
+              : "None"}
+          </p>
+          <button
+            type="button"
+            className="btn btn-warning btn-sm"
+            onClick={() => props.setHeavenMaster([])}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col m-2 p-2 border border-dark">
+          <h3>Rules</h3>
+          {props.rules.length === 0 ? (
+            <p>None</p>
+          ) : (
+            <ul>
+              {props.rules.map((r) => (
+                <li>{r}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="col m-2 p-2 border border-dark">
+          <h3>Mates</h3>
+          {props.teams.length === 0 ? (
+            <p>None</p>
+          ) : (
+            <ul>
+              {props.teams.map((t) => (
+                <li>{getTeamMemberNames(t)}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
