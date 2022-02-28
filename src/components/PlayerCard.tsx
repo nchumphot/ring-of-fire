a0cc9a4851000e11c7235dab5b21b751e7abc757
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-02-28 11:24:31 +0000

    Added edit and delete buttons in a player card

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -3,7 +3,21 @@ import { IPlayer } from "../interfaces/IPlayer";
 export function PlayerCard(props: { player: IPlayer }): JSX.Element {
   return (
     <div className="border border-success m-2 p-2">
-      <h3>{props.player.name}</h3>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-10">
+            <h3>{props.player.name}</h3>
+          </div>
+          <div className="col">
+            <button type="button" className="btn btn-warning btn-sm me-2">
+              Edit
+            </button>
+            <button type="button" className="btn btn-danger btn-sm">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
